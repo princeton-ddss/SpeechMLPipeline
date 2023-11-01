21bc690f8f896562d2b9ed5acc85824bd80c0741
fjying <jf3375@princeton.edu>
2023-10-31 23:01:44 -0700

    Check and Remove invalid processed file to ensure the consistency of files across folders

--- src/PreprocessData/VideoAudioPreprocessing.py
+++ src/PreprocessData/VideoAudioPreprocessing.py
@@ -52,12 +52,17 @@ def cut_video_and_audio_based_on_silence(input_filename_noft, video_filetype, au
     output_video_file = "{}/video/{}.{}".format(output_path, input_filename_noft, video_filetype)
 
     silence_file = "{}/{}.{}".format(input_silencets_path, input_silence_filename)
-    if os.path.getsize(silence_file) == 0:
-        print("No Silence TimeStamp")
-        shutil.copyfile(input_file, output_video_file)
-        audio = AudioFileClip(input_file)
-        audio.write_audiofile(output_audio_file)
-        return
+
+    #  Check and Remove invalid processed file to ensure the consistency of files across folders
+    try:
+        video = VideoFileClip(input_file, audio_buffersize=400000)
+        duration = video.duration
+    except:
+        print('error')
+        os.remove(input_file)
+
+
+
     
     in_handle = open('{}/{}'.format(input_silencets_path, input_silence_filename), "r", errors='replace')
 
