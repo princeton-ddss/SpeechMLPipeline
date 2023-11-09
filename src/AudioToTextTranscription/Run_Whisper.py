d59fea7443ad44f3eac12d47af32919d21fba7da
fjying <jf3375@princeton.edu>
2023-11-08 19:20:51 -0500

    Set inputs path on Whisper

--- src/AudioToTextTranscription/Run_Whisper.py
+++ src/AudioToTextTranscription/Run_Whisper.py
@@ -1,23 +1,26 @@
 import os
-from joblib import Parallel, delayed
 
 from Whisper import whisper_transcription
 
 def main():
     # Sound File Input Path
-    soundfile_input_path = '/Users/jf3375/Desktop/DDSS/Projects/NJFS/audio_speech/data/njfs/output_cut/audio'
+    # soundfile_input_path = '/Users/jf3375/Desktop/DDSS/Projects/NJFS/audio_speech/data/njfs/output_cut/audio'
+    soundfile_input_path = '/drives/Njfs-Audio/output/preprocess_data/output_cut/1007/audio'
 
     # Model path
-    model_path = '/Users/jf3375/PycharmProjects/TestMLEnvironment/models/Whisper/large-v2.pt'
+    # model_path = '/Users/jf3375/PycharmProjects/TestMLEnvironment/models/Whisper/large-v2.pt'
+    model_path = '/drives/Njfs-Audio/models/Whisper/large-v2.pt'
 
     # Output path
-    output_path = '/Users/jf3375/Desktop/DDSS/Projects/NJFS/audio_speech/data/njfs/whisper_result'
+    output_path = '/drives/Njfs-Audio/output/whisper_result'
 
     os.chdir(soundfile_input_path)
 
-    Parallel(n_jobs=-1)(delayed(whisper_transcription)(input_filename, model_path, output_path)
-                        for input_filename in os.listdir(soundfile_input_path)
-                                    if input_filename.endswith('.wav'))
+    for input_filename in os.listdir(soundfile_input_path):
+        if input_filename.endswith('.wav'):
+            whisper_transcription(input_filename, model_path, output_path)
+
+
 
 
 if __name__ == '__main__':
